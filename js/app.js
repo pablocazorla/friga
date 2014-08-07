256751071abe4b0f5e930de9e18c105d0c6e8c0b
Pablo Cazorla <pablo.david.cazorla@gmail.com>
2014-08-07 13:11:20 -0300

    listo slider

--- js/app.js
+++ js/app.js
@@ -693,33 +693,49 @@ pc.contentSlider = {
 				$controls = $('<div class="slider-controls"></div>'),
 				$controlsSpans = $(),
 				showing = false,
-				change = function(num){
+				$arrowLeft = $('<div class="slider-arrow to-left"><span></span></div>'),
+				$arrowRight = $('<div class="slider-arrow to-right"><span></span></div>'),
+				calculateHeight = function(){
+					$sliderContent.css('height',$imgs.eq(current).height()+'px');
+				},
+				change = function(num,autostop){
 					if(num != current && !showing){
+						if(autostop && timer != null){
+							clearInterval(timer);timer = null;
+						}
 						showing = true;
+						if(num < 0){num = length - 1;}
+						if(num >= length){num = 0;}
 						postCurrent = current;
 						current = num;
 						$imgs.eq(current).addClass('current');
 						$imgs.eq(postCurrent).removeClass('current').addClass('post-current');
 						$controlsSpans.eq(current).addClass('current');
 						$controlsSpans.eq(postCurrent).removeClass('current');
-						$sliderContent.css('height',$imgs.eq(current).height()+'px');
+						calculateHeight();
 						setTimeout(function(){
 							$imgs.removeClass('post-current');
 							showing = false;
 						},790);
 					}
-				}
+				},				
+				duration = 7000,
+				timer = setInterval(function(){
+					change(current+1,false);
+				},duration);
 
-			$slider.html('').append($sliderContent.append($imgs)).append($controls);
-			
+			$slider.html('').append($sliderContent.append($imgs)).append($controls).append($arrowLeft).append($arrowRight);		
 
 			for(var i = 0;i< length;i++){
 				$('<span title="'+$imgs.eq(i).attr('alt')+'" data-ind="'+i+'"></span>').appendTo($controls).click(function(){
-					change(parseInt($(this).attr('data-ind')));
+					change(parseInt($(this).attr('data-ind')),true);
 				});
 			}
 			$controlsSpans = $controls.find('span');
+			$arrowLeft.click(function(){change(current-1,true);});
+			$arrowRight.click(function(){change(current+1,true);});
 			change(0);
+			pc.$window.resize(calculateHeight);
 		});
 	}
 };
